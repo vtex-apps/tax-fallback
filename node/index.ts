b6a6f4c632c6b88fc823f4b86d94891f28a6e2a0
polishq <arthur.bond@vtex.com.br>
2020-12-09 16:20:06 -0500

    Throw 404 if no taxes are found

--- node/index.ts
+++ node/index.ts
@@ -6,7 +6,6 @@ import {
   ServiceContext,
   RecorderState,
 } from '@vtex/api'
-// import * as parse from 'csv-parse/lib/sync'
 
 import { Clients } from './clients'
 import {
@@ -16,23 +15,17 @@ import {
 
 const TIMEOUT_MS = 10000
 
-// Create a LRU memory cache for the Status client.
-// The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
 const memoryCache = new LRUCache<string, any>({ max: 5000 })
 
 metrics.trackCache('tax-fallback', memoryCache)
 
-// This is the configuration for clients available in `ctx.clients`.
 const clients: ClientsConfig<Clients> = {
-  // We pass our custom implementation of the clients bag, containing the Status client.
   implementation: Clients,
   options: {
-    // All IO Clients will be initialized with these options, unless otherwise specified.
     default: {
       retries: 2,
       timeout: TIMEOUT_MS,
     },
-    // This key will be merged with the default options and add this cache to our Status client.
     avalara: {
       memoryCache,
     },
